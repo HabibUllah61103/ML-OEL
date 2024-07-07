0a3dd7f36adcf566bd94ff6ab66f29f550d81349
HabibUllah61103 <hu61103@gmail.com>
2024-07-08 01:29:07 +0500

    Completed

--- StockEzy/StockEzyApp/urls.py
+++ StockEzy/StockEzyApp/urls.py
@@ -2,10 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("customer_signup", views.customer_signup, name="customer_signup"),
+    path("", views.customer_signup, name="customer_signup"),
     path("customer_signin", views.customer_signin, name="customer_signin"),
-    path("customer_profile/<int:customer_id>", views.customer_profile, name="customer_profile"),
+    path(
+        "customer_profile/<int:customer_id>",
+        views.customer_profile,
+        name="customer_profile",
+    ),
     path("dashboad_view/<int:customer_id>", views.dashboad_view, name="dashboard_view"),
     path("history_view/<int:customer_id>", views.history_view, name="history_view"),
-    #path("license_view>", views.license_view, name="license_view"),
+    # path("license_view>", views.license_view, name="license_view"),
 ]
